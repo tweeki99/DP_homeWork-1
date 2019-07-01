621dc427ef64f5fbcc5b8dce286eecd3bb54c213
Данил Шипилов <danil.shipilov.99@gmail.com>
2019-07-01 20:41:54 +0600

    DpHw-1

---
+++ DpHw-1.Computer/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_1.Computer
+{
+    public interface IFactoryComputer
+    {
+        IAbstractProcessor CreateProcessor();
+
+        IAbstractMainboard CreateMainboard();
+    }
+
+    class Sumsung : IFactoryComputer
+    {
+        public IAbstractProcessor CreateProcessor()
+        {
+            return new ProcessorBySony();
+        }
+
+        public IAbstractMainboard CreateMainboard()
+        {
+            return new MainboardBySony();
+        }
+    }
+
+    class Dell : IFactoryComputer
+    {
+        public IAbstractProcessor CreateProcessor()
+        {
+            return new ProcessorByDell();
+        }
+
+        public IAbstractMainboard CreateMainboard()
+        {
+            return new MainboardByDell();
+        }
+    }
+
+    public interface IAbstractProcessor
+    {
+        string ProcessorInfo();
+    }
+
+    class ProcessorBySony : IAbstractProcessor
+    {
+        public string ProcessorInfo()
+        {
+            return "The result of the product ProcessorBySony.";
+        }
+    }
+
+    class ProcessorByDell : IAbstractProcessor
+    {
+        public string ProcessorInfo()
+        {
+            return "The result of the product ProcessorByDell.";
+        }
+    }
+
+    public interface IAbstractMainboard
+    {
+        string MainboardInfo();
+
+        string AnotherUsefulFunctionB(IAbstractProcessor collaborator);
+    }
+
+    class MainboardBySony : IAbstractMainboard
+    {
+        public string MainboardInfo()
+        {
+            return "The result of the product MainboardBySony.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractProcessor collaborator)
+        {
+            var result = collaborator.ProcessorInfo();
+
+            return $"The result of the MainboardBySony collaborating with the ({result})";
+        }
+    }
+
+    class MainboardByDell : IAbstractMainboard
+    {
+        public string MainboardInfo()
+        {
+            return "The result of the product MainboardByDell.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractProcessor collaborator)
+        {
+            var result = collaborator.ProcessorInfo();
+
+            return $"The result of the MainboardByDell collaborating with the ({result})";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Client: Testing client code with the Sony factory type...");
+            ClientMethod(new Sumsung());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing the same client code with the Dell factory type...");
+            ClientMethod(new Dell());
+        }
+
+        public void ClientMethod(IFactoryComputer factory)
+        {
+            IAbstractProcessor processor = factory.CreateProcessor();
+            IAbstractMainboard mainboard = factory.CreateMainboard();
+
+            Console.WriteLine(mainboard.MainboardInfo());
+            Console.WriteLine(mainboard.AnotherUsefulFunctionB(processor));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DpHw-1.car/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_1.Car
+{
+    public interface IFactoryCar
+    {
+        IAbstractEngine CreateEngine();
+
+        IAbstractBody CreateBody();
+    }
+
+    class Sumsung : IFactoryCar
+    {
+        public IAbstractEngine CreateEngine()
+        {
+            return new EngineByFord();
+        }
+
+        public IAbstractBody CreateBody()
+        {
+            return new BodyByFord();
+        }
+    }
+
+    class Toyota : IFactoryCar
+    {
+        public IAbstractEngine CreateEngine()
+        {
+            return new EngineByToyota();
+        }
+
+        public IAbstractBody CreateBody()
+        {
+            return new BodyByToyota();
+        }
+    }
+
+    public interface IAbstractEngine
+    {
+        string EngineInfo();
+    }
+
+    class EngineByFord : IAbstractEngine
+    {
+        public string EngineInfo()
+        {
+            return "The result of the product EngineByFord.";
+        }
+    }
+
+    class EngineByToyota : IAbstractEngine
+    {
+        public string EngineInfo()
+        {
+            return "The result of the product EngineByToyota.";
+        }
+    }
+
+    public interface IAbstractBody
+    {
+        string BodyInfo();
+
+        string AnotherUsefulFunctionB(IAbstractEngine collaborator);
+    }
+
+    class BodyByFord : IAbstractBody
+    {
+        public string BodyInfo()
+        {
+            return "The result of the product BodyByFord.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractEngine collaborator)
+        {
+            var result = collaborator.EngineInfo();
+
+            return $"The result of the BodyByFord collaborating with the ({result})";
+        }
+    }
+
+    class BodyByToyota : IAbstractBody
+    {
+        public string BodyInfo()
+        {
+            return "The result of the product BodyByToyota.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractEngine collaborator)
+        {
+            var result = collaborator.EngineInfo();
+
+            return $"The result of the BodyByToyota collaborating with the ({result})";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Client: Testing client code with the Ford factory type...");
+            ClientMethod(new Sumsung());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing the same client code with the Toyota factory type...");
+            ClientMethod(new Toyota());
+        }
+
+        public void ClientMethod(IFactoryCar factory)
+        {
+            IAbstractEngine engine = factory.CreateEngine();
+            IAbstractBody body = factory.CreateBody();
+
+            Console.WriteLine(body.BodyInfo());
+            Console.WriteLine(body.AnotherUsefulFunctionB(engine));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+            Console.ReadLine();
+        }
+    }
+}
